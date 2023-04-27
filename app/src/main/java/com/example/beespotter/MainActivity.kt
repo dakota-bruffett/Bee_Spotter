5c04b2b46a38720b58183a9296a196c43185618d
Alicia <Alicia@gmail.com>
2023-04-27 15:55:01 -0500

    First commit

--- app/src/main/java/com/example/beespotter/MainActivity.kt
+++ app/src/main/java/com/example/beespotter/MainActivity.kt
@@ -1,11 +1,47 @@
 package com.example.beespotter
 
+import android.content.ContentValues.TAG
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.ImageButton
+import com.google.firebase.ktx.Firebase
+import java.util.Date
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var btnMap: ImageButton
+    private lateinit var btnBee: ImageButton
+    private lateinit var btnBack:Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnMap = findViewById(R.id.btn_Map)
+        btnBack = findViewById(R.id.btn_Back)
+        btnBee = findViewById(R.id.btn_Bee)
+
+        val fragment =
+            supportFragmentManager.findFragmentById(R.id.fragment_container)
+
+
+//            .commit {
+//            .replace<ExampleFragment>(R.id.fragment_container)
+//            setReorderingAllowed(true)
+//            addToBackStack(null)
+
+
+
+            val db = Firebase.firestore
+        val bee = mapOf("name" to "bee", "dateSpotted" to Date())
+
+        db.collection("bees").get().addOnSuccessListener { beeDocuments ->
+            for ( beeDoc in beeDocuments) {
+                val name = beeDoc["name"]
+                val dateSpotted = treeDoc["dateSpotted"]
+                Log.d(TAG, "$name, $dateSpotted")
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/example/beespotter/MapFragment.kt
@@ -0,0 +1,40 @@
+package com.example.beespotter
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+
+class MapFragment : Fragment() {
+    // TODO: Rename and change types of parameters
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_map, container, false)
+    }
+
+    companion object {
+
+        @JvmStatic
+        fun newInstance(param1: String, param2: String) =
+            MapFragment().apply {
+                arguments = Bundle().apply {
+
+                }
+            }
+    }
+}
