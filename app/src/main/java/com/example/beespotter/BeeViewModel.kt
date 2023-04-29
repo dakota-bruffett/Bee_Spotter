437d8cbb3d975e9d89835d120791f5604662541b
dan <email@email.com>
2023-04-28 22:11:49 -0500

    Design MainActivity - in progress

---
+++ app/src/main/java/com/example/beespotter/BeeViewModel.kt
@@ -0,0 +1,20 @@
+package com.example.beespotter
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+
+class BeeViewModel: ViewModel() {
+    private val db = Firebase.firestore
+    private val beeCollectionReference = db.collection("bees")
+
+    val latestBees = MutableLiveData<List<Bee>>() // constructor
+
+
+    fun addBee(bee: Bee) {
+        TODO("Not yet implimented")
+    }
+
+    // TODO Other Additions as needed.
+}
