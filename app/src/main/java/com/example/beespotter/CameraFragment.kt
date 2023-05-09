094b8ed102ef300c39308faaba3197039ff4e21e
nq2623up <DakotaBruffett@gmail.com>
2023-05-09 12:19:38 -0500

    Merge branch 'recycler_view' of https://github.com/dakota-bruffett/Bee_Spotter into Picture
     Conflicts:
    	app/build.gradle

--- app/src/main/java/com/example/beespotter/CameraFragment.kt
+++ app/src/main/java/com/example/beespotter/CameraFragment.kt
@@ -1,8 +1,6 @@
 package com.example.beespotter
 
-import android.annotation.SuppressLint
 import android.content.ContentValues.TAG
-import android.content.Context
 import android.content.Intent
 import android.icu.text.SimpleDateFormat
 import android.net.Uri
@@ -10,7 +8,6 @@ import android.os.Bundle
 import android.os.Environment
 import android.provider.MediaStore
 import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -20,6 +17,7 @@ import androidx.activity.result.ActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.FileProvider
+import androidx.fragment.app.Fragment
 import com.google.android.material.snackbar.Snackbar
 import com.google.firebase.ktx.Firebase
 import com.google.firebase.storage.ktx.storage
@@ -27,7 +25,6 @@ import com.squareup.picasso.Callback
 import com.squareup.picasso.Picasso
 import java.io.File
 import java.io.IOException
-import java.lang.Exception
 import java.util.*
 
 
